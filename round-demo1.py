d9c847084e8ac47ed4e835f6054b867fc7b95b54
Jawesome99 <Jan.Dassow@gmx.de>
2015-12-15 20:21:15 +0100

    Uploaded round-demo1.py
    Very WIP, literally nothing is finished.
    
    Hell, this is just a placeholder yet.
    
    Go see master branch for working stuff.

---
+++ round-demo1.py
@@ -0,0 +1,11 @@
+# Testing the functionality of a simple round. Moving, actions and such
+# No enemys, AI is in concept phase
+# No buymenu, gear is set
+# Only t-sided. Teamswitch will be added in a later demo
+# Damagetests in concept phase
+
+# VERY incomplete
+# This is on dev for a reason
+# Go view the master branch for working stuff instead
+
+wlist = ['0. Knife', '1. Glock 18', '2. AK-47'] # Default gear for this test
